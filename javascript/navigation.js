8aa00c7cb3cad9da96de15e1a9e8e1039f051fed
Jonas Voetmann <48560462+JonasVoet@users.noreply.github.com>
2019-10-11 22:54:47 +0200

    ddd

---
+++ javascript/navigation.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    $(document).ready(function () {
+        $('.toggle').click(function () {
+            $('.toggle').toggleClass('active')
+            $('.sidebar').toggleClass('active')
+        })
+    })
+
+});
+
+
